ed5add40e57e40feb7eff010bee0ec8eaf582a90
rbenrayana <rbenrayana@olfeo.com>
2015-07-20 13:40:26 +0200

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='buildall',
+      version='0.1',
+      description='Build automation tool',
+      author='Rayene BEN RAYANA',
+      author_email='rayene.benrayana@gmail.com',
+      url='',
+      #packages=['buildall'],
+)
